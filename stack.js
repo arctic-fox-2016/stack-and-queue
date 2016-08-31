8a435611abbac1ed30b86b53d3451f36fdd69435
Andrew Tandiawan & Elkana Lumbantobing <pair+atandiawan.elkana-ltobing@hacktiv8.com>
2016-08-31 15:47:14 +0700

    Tambah Release 2

--- stack.js
+++ stack.js
@@ -19,20 +19,67 @@ class Stack {
 
   isEmpty() {
     if(this.temp.length > 0){
-      return false;
+      return "tidak kosong";
     } else {
-      return true;
+      return "kosong";
     }
+  }
+
+  size(){
+    return this.temp.length
   }
 }
 
-let piring = new Stack();
-piring.push("piring kotor");
-piring.push("piring bersih");
-piring.push("piring");
-piring.peek()
+class Queue {
+  constructor() {
+    this.temp = []
+  }
 
+  push(x) {
+    this.temp.push(x)
+  }
+
+  pop() {
+    this.temp.shift()
+  }
+
+  peek() {
+    console.log(this.temp)
+  }
+
+  isEmpty() {
+    if(this.temp.length > 0){
+      return "tidak kosong";
+    } else {
+      return "kosong";
+    }
+  }
+
+  size(){
+    return this.temp.length
+  }
+}
+
+
+let piring = new Stack();
+piring.push("piring satu");
+piring.push("piring dua");
+piring.push("piring tiga");
+piring.push("piring empat");
+piring.push("piring lima");
+piring.peek()
 piring.pop();
 piring.peek();
 
 console.log(piring.isEmpty())
+
+let antri = new Queue();
+antri.push("masuk pertama");
+antri.push("masuk kedua");
+antri.push("masuk ketiga");
+antri.peek()
+
+antri.pop();
+antri.peek();
+
+console.log(antri.isEmpty())
