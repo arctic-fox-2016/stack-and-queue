8ac9a1e8b2398969bf306b7c40347b0814298f98
Ryan Andhaka & Toni <pair+ryanandhaka.tonychen09@hacktiv8.com>
2016-08-31 16:36:58 +0700

    stack

--- stack.js
+++ stack.js
@@ -2,18 +2,54 @@
 
 class Stack {
   constructor() {
+      this.tampung = new Array()
+      this.max = 10
   }
 
   push(x) {
+    if (this.tampung.length <= 10){
+      this.tampung.push(x);
+      return this.tampung;
+    }
   }
 
   pop() {
+    this.tampung.pop();
+    return this.tampung;
   }
 
   peek() {
+    this.tampung.peek();
+    return this.tampung;
 
   }
 
   isEmpty() {
+  if(this.max.length == 0){
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+  }
+
+  isFull()
+  {
+    if(this.max.length > 10)
+    {
+      return false;
+    }
   }
 }
+
+let my_stack = new Stack()
+
+//console.log(`The stack is empty? ${stack.isEmpty()}`)
+
+my_stack.push("Javascript")
+my_stack.push("is just so")
+my_stack.push(15)
+my_stack.pop()
+my_stack.peek();
+console.log(my_stack.push())
